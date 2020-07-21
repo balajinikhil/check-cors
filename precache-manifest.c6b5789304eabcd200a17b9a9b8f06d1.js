40b6fc7f25268acb2fb0eb0affd70102dca9bcdf
balajinikhil <balajiedward007@gmail.com>
2020-07-22 02:49:38 +0530

    Updates

---
+++ precache-manifest.c6b5789304eabcd200a17b9a9b8f06d1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01e356e25c60138fcc3e3c2ed04c6e25",
+    "url": "/check-cors/index.html"
+  },
+  {
+    "revision": "b36f74a82784752dd97f",
+    "url": "/check-cors/static/js/2.9e9514b0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/check-cors/static/js/2.9e9514b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "199f2b64ddb1df15d631",
+    "url": "/check-cors/static/js/main.7348661b.chunk.js"
+  },
+  {
+    "revision": "7610a5d4951357977c22",
+    "url": "/check-cors/static/js/runtime-main.0f1d936d.js"
+  }
+]);
